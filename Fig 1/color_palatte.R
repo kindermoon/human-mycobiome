9f1a0c3ec5412cc46cc17ddb023f541bafecd871
kindermoon <hjing1002@gmail.com>
2026-03-24 08:41:17 +0800

    Add files via upload
    color palatte

---
+++ Fig 1/color_palatte.R
@@ -0,0 +1,10 @@
+#!/usr/bin/env Rscript
+
+library(ggsci)
+# Available color palatte in ggsci package
+palname <- c("npg","aaas","nejm","lancet","jama","jco","ucscgb","d3",
+	     "locuszoom","igv","uchicago","startrek","tron","futurama",
+	     "rickandmorty","simpsons","gsea","material")
+scale_color_palname()
+scale_fill_palname()
+
